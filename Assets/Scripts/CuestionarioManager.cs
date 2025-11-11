a95e98fe3d5c090ee190bd9cfe12d28a548ed585
ron romero <ron@Dereks-MacBook-Pro.local>
2025-11-11 15:49:24 -0300

    se mezclan las respuestas en el cuestionario

--- Assets/Scripts/CuestionarioManager.cs
+++ Assets/Scripts/CuestionarioManager.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq; 
 
 public class CuestionarioManager : MonoBehaviour
 {
     [Header("UI")]
     [SerializeField] private TMP_Text textoPregunta;
     [SerializeField] private Button[] botonesOpciones;
-    [SerializeField] private TMP_Text feedbackTexto; 
+    [SerializeField] private TMP_Text feedbackTexto;
     [SerializeField] private Button botonVolverMenu;
-    [SerializeField] private Button botonReintentar; 
+    [SerializeField] private Button botonReintentar;
     [SerializeField] private GameObject panelCuestionario;
 
     private Pregunta[] preguntas;
@@ -73,18 +74,23 @@ public class CuestionarioManager : MonoBehaviour
         textoPregunta.text = p.texto;
         feedbackTexto.text = "";
 
+        var opcionesMezcladas = p.opciones
+            .Select((texto, indice) => new { texto, indice })
+            .OrderBy(x => Random.value)
+            .ToArray();
+
         for (int i = 0; i < botonesOpciones.Length; i++)
         {
-            if (i < p.opciones.Length)
+            if (i < opcionesMezcladas.Length)
             {
                 botonesOpciones[i].gameObject.SetActive(true);
-                botonesOpciones[i].GetComponentInChildren<TMP_Text>().text = p.opciones[i];
+                botonesOpciones[i].GetComponentInChildren<TMP_Text>().text = opcionesMezcladas[i].texto;
                 botonesOpciones[i].interactable = true;
                 botonesOpciones[i].GetComponent<Image>().color = Color.white;
-
                 botonesOpciones[i].onClick.RemoveAllListeners();
-                int opcion = i;
-                botonesOpciones[i].onClick.AddListener(() => EvaluarRespuesta(opcion));
+                int indiceOriginal = opcionesMezcladas[i].indice;
+
+                botonesOpciones[i].onClick.AddListener(() => EvaluarRespuesta(indiceOriginal));
             }
             else
             {
@@ -99,7 +105,6 @@ public class CuestionarioManager : MonoBehaviour
         Color verdeClarito = new Color(0.6f, 1f, 0.6f);
         Color rojoClarito = new Color(1f, 0.6f, 0.6f);
 
-        // Desactivar todos los botones
         foreach (var btn in botonesOpciones)
             btn.interactable = false;
 
@@ -108,7 +113,10 @@ public class CuestionarioManager : MonoBehaviour
             feedbackTexto.text = "¡Correcto!";
             feedbackTexto.color = Color.green;
 
-            botonesOpciones[seleccion].GetComponent<Image>().color = verdeClarito;
+            Button btnCorrecto = botonesOpciones.FirstOrDefault(b => 
+                b.GetComponentInChildren<TMP_Text>().text.Trim() == p.opciones[p.respuestaCorrecta].Trim());
+            if (btnCorrecto != null)
+                btnCorrecto.GetComponent<Image>().color = verdeClarito;
 
             gameManager.GanarExperiencia(1);
             if (botonVolverMenu != null) botonVolverMenu.gameObject.SetActive(true);
@@ -118,7 +126,10 @@ public class CuestionarioManager : MonoBehaviour
             feedbackTexto.text = "Incorrecto. ¡Intentá de nuevo!";
             feedbackTexto.color = Color.red;
 
-            botonesOpciones[seleccion].GetComponent<Image>().color = rojoClarito;
+            Button btnSeleccionado = botonesOpciones.FirstOrDefault(b =>
+                b.GetComponentInChildren<TMP_Text>().text.Trim() == p.opciones[seleccion].Trim());
+            if (btnSeleccionado != null)
+                btnSeleccionado.GetComponent<Image>().color = rojoClarito;
 
             if (botonReintentar != null) botonReintentar.gameObject.SetActive(true);
         }
